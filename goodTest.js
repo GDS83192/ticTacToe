ad098457c73e74abcb3f0e112f54c95d63848682
gds83192 <graham.squires3@gmail.com>
2020-10-07 18:42:47 -0400

    box X or O functionality implemented, next need to make user class

--- goodTest.js
+++ goodTest.js
@@ -44,3 +44,21 @@ board.setAttribute('id', 'board');
 //                     "   |    |     |"
 //                     "-----------------"
 //                     "   |    |     |" */
+
+function userMoves() {
+    const box1 = document.getElementById('box1')
+    const box2 = document.getElementById('box2')
+    const box3 = document.getElementById('box3')
+    const box4 = document.getElementById('box4')
+    const box5 = document.getElementById('box5')
+    const box6 = document.getElementById('box6')
+    const box7 = document.getElementById('box7')
+    const box8 = document.getElementById('box8')
+    const box9 = document.getElementById('box9')
+
+    box1.addEventListener('click', () => {
+        box1.innerHTML = 'X'
+    })
+}
+
+userMoves();
